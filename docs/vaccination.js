30514058fc45aca5e1586567c3fcd34715738c95
RemiFontan2 <remifontann@gmail.com>
2021-01-10 20:12:43 +0100

    erreur bouton

--- docs/vaccination.js
+++ docs/vaccination.js
@@ -62,10 +62,10 @@ const vaccination = {
     e.preventDefault();
 
     vaccination.add(
-      $('#name').trim(),
-      $('#email').trim(),
-      $('#comment').trim(),
-      $('#vaccination_date').trim()
+      $('#name').accessKey,
+      $('#email').accessKey,
+      $('#comment').accessKey,
+      $('#vaccination_date').accessKey
     ).done(function(result) {
       // reload entries
       loadEntries();
