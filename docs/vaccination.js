1d7013c1874b156b5640dd8c3c13cd6d3bd5346e
RemiFontan2 <remifontann@gmail.com>
2021-01-11 14:53:43 +0100

    test validation formulaire

--- docs/vaccination.js
+++ docs/vaccination.js
@@ -62,10 +62,10 @@ const vaccination = {
     e.preventDefault();
 
     vaccination.add(
-      $('#name').val().trim(),
-      $('#email').val().trim(),
-      $('#comment').val().trim(),
-      $('#vaccination_date').val().trim()
+      $('#name').trim,
+      $('#email').trim,
+      $('#comment').trim,
+      $('#vaccination_date').trim
     ).done(function(result) {
       // reload entries
       loadEntries();
